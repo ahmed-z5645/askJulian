04ca2013853ed9042b31521020fbcdb9b7eba428
ahmed-z5645 <ahmed.zafar5645@gmail.com>
2025-03-20 22:24:40 -0400

    front-end is *almost* done, just need to make api reqs

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,48 +2,72 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Background from './background'
+import axois from "axios"
 
 function App() {
   const [count, setCount] = useState(0)
-  const [isSong, setIsSong] = useState(true) //treu ? search song : search albums
+  const [isSong, setIsSong] = useState(false) //true ? search song : search albums
 
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log('submit')
   }
   return (
-    <>
-      <h1>Hey, I'm Julian</h1>
-      <p>Welcome to askjulian.xyz ! I'm glad you could make it. 
-        <br />
-        Select "Song" or "Album" below, and then I'll let you know what I think of it, and some songs that are similar (and probably better) 
-      </p>
-      <div className="song-album">
-        <button onClick={() => setIsSong(true)}
-          style = {{backgroundColor: isSong ? "#3b3b3b" : "#1a1a1a", 
-                    borderTopRightRadius: "0px", borderBottomRightRadius: "0px"
-          }}
-          >song</button>
-        <button onClick={() => setIsSong(false)}
-          style = {{backgroundColor: isSong ? "#1a1a1a" : "#3b3b3b",
-                    borderTopLeftRadius: "0px", borderBottomLeftRadius: "0px"
-          }}
-          >album</button>
+    <div className="app">
+      <div className="background">
+        <Background />
       </div>
-      <div className="Form">
-        <h3>Am I valid?</h3>
-        <form onSubmit={handleSubmit}
-          style={{display: "flex", flexDirection: "column",}}>
-          <input
-            type="text"
-            placeholder={isSong ? "Song" : "Album"}></input>
-          <input
-            type="text"
-            placeholder="Artist"></input>
-          <button type="submit">Roast Me</button>
-        </form>
+      <div className="content">
+        <div className="title">
+          <h1>julian</h1>
+          <h3>Your Programmed Music <br />Enthusiast</h3>
+          <p>I'm glad you made it. I'm Julian, an artificial intelligence model designed to have a better in music taste than you. 
+            <br />Give me an album or song, and I'll let you know what I think about it</p>
+        </div>
+        <div className="func">
+        <div className="song-album">
+          <button onClick={() => setIsSong(true)}
+            style = {{backgroundColor: isSong ? "#3b3b3b" : "#1a1a1a", 
+                      borderTopRightRadius: "0px", borderBottomRightRadius: "0px", width: "20%"
+            }}
+            >Song</button>
+          <button onClick={() => setIsSong(false)}
+            style = {{backgroundColor: isSong ? "#1a1a1a" : "#3b3b3b", width: "20%",
+                      borderTopLeftRadius: "0px", borderBottomLeftRadius: "0px"
+            }}
+            >Album</button>
+        </div>
+        {(isSong) ? 
+        <div>
+          <h3 style ={{fontWeight: "400"}}>Yeah, right. Go listen to an <span style={{fontFamily: "Borel", color: "#e59500"}}>album</span>, the way the music was <span style={{fontFamily: "Borel", color: "#e59500"}}>supposed to be experienced. </span></h3>
+          <p>I'm just kidding, coming soon :)</p>
+        </div>
+        :
+        <div className="Form">
+          <p>Are You Valid?</p>
+          <form onSubmit={handleSubmit}
+            style={{display: "flex", flexDirection: "column", width: "80%", margin: "auto"}}>
+            <label className="formLabel">Album</label>
+            <input
+              type="text"
+              placeholder="Example: Sling" 
+              className="formInput"
+              name="album"
+              required></input>
+            <label className="formLabel">Artist</label>
+            <input
+              type="text"
+              placeholder="Example: Clairo" 
+              className="formInput"
+              name="album"
+              required></input>
+            <button type="submit">Roast Me</button>
+          </form>
+        </div>}
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
